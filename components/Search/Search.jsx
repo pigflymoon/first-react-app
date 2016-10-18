7b3fe553c68a896f67a9ab23bfabb1a6c947f47c
pigflymoon <pplflymoon2015@gmail.com>
2016-10-19 12:08:59 +1300

    #my first app #Search didn't work

--- components/Search/Search.jsx
+++ components/Search/Search.jsx
@@ -13,6 +13,7 @@ export default class Search extends React.Component {
 
     handleSubmit(event) {
 
+        console.log('hi');
         event.preventDefault();
 
         // When the form is submitted, call the onSearch callback that is passed to the component
@@ -26,12 +27,12 @@ export default class Search extends React.Component {
     render() {
 
         return (
-            <form id="geocoding_form" className="form-horizontal" onSubmit={this.handleSubmit}>
+            <form id="geocoding_form" className="form-horizontal" onSubmit={this.handleSubmit.bind(this)}>
                 <div className="form-group">
                     <div className="col-xs-12 col-md-6 col-md-offset-3">
                         <div className="input-group">
                             <input type="text" className="form-control" id="address" placeholder="Find a location..."
-                                   value={this.state.value} onChange={this.handleChange}/>
+                                   value={this.state.value} onChange={this.handleChange.bind(this)}/>
 							<span className="input-group-btn">
 								<span className="glyphicon glyphicon-search" aria-hidden="true"></span>
 							</span>
